fa04a1c20fa359c1a356bbd80052e0c79041ee6f
oji <yo-00331pc@outlook.com>
2025-05-23 00:41:46 +0900

    change variable from x to i

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,10 +1,10 @@
-(1..20).each do |x|
-  if x % 15 == 0
+(1..20).each do |i|
+  if i % 15 == 0
     puts "FizzBuzz"
-  elsif x % 3 == 0
+  elsif i % 3 == 0
     puts "Fizz"
-  elsif x % 5 == 0
+  elsif i % 5 == 0
     puts "Buzz"
-  else puts x
+  else puts i
   end
 end
