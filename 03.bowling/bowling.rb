43d484c99cc2b40c58a67c07d4914efc6d54160e
oji <yo-00331pc@outlook.com>
2025-06-01 04:45:52 +0900

    add strike and spare to points

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,16 +1,9 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 score = ARGV[0]
 scores = score.split(',')
-shots = []
-
-scores.each do |s|
-  if s == 'X'
-    shots << 10
-  else
-    shots << s.to_i
-  end
-end
+shots = scores.map { |s| s == 'X' ? 10 : s.to_i }
 
 frames = []
 i = 0
@@ -25,26 +18,28 @@ while i < shots.size && frames.size < 9
   end
 end
 
-frame10 = []
-
-if i < shots.size
-  frame10.push(shots[i])
-end
-
-if i + 1 < shots.size
-  frame10.push(shots[i + 1])
-end
-
-if (frame10[0] == 10) || (frame10[0] + frame10[1] == 10)
-  if i + 2 < shots.size
-    frame10.push(shots[i + 2])
-  end
-end
-
+frame10 = [shots[i], shots[i + 1]]
+frame10 << shots[i + 2] if shots[i + 2] && (frame10[0] == 10 || frame10[0].to_i + frame10[1].to_i == 10)
 frames.push(frame10)
 
 point = 0
-frames.each do |frame|
-  point += frame.sum
+
+frames.each_with_index do |frame, index|
+  if index == 9
+    point += frame.sum
+  elsif frame[0] == 10
+    next_frame = frames[index + 1]
+    if next_frame[0] == 10 && index + 2 < frames.size
+      next_next_frame = frames[index + 2]
+      point += 10 + 10 + next_next_frame[0]
+    else
+      point += 10 + next_frame[0] + next_frame[1]
+    end
+  elsif frame.sum == 10
+    next_frame = frames[index + 1]
+    point += 10 + next_frame[0]
+  else
+    point += frame.sum
+  end
 end
 puts point
