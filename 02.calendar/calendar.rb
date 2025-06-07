b881cb3bfc619220b92221194a3600c50d6ad330
oji <yo-00331pc@outlook.com>
2025-06-08 00:52:24 +0900

    change day to date to make code more appropriate

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -12,22 +12,22 @@ OptionParser.new do |opts|
   opts.on("-m ",Integer) { |m| month = m }
 end.parse!
 
-first_day = Date.new(year, month, 1)
-last_day = Date.new(year, month, -1)
+first_date = Date.new(year, month, 1)
+last_date = Date.new(year, month, -1)
 
 puts "#{month}月 #{year}".center(20)
 puts "日 月 火 水 木 金 土"
 
-first_wday = first_day.wday
+first_wday = first_date.wday
 print "   " * first_wday
 
-(first_day..last_day).each do |day|
-  day_str = day.day.to_s.rjust(2)
-  print day == today ? "\e[7m#{day_str}\e[0m " : "#{day_str} "
+(first_date..last_date).each do |date|
+  day_str = date.day.to_s.rjust(2)
+  print date == today ? "\e[7m#{day_str}\e[0m " : "#{day_str} "
 
-  puts if day.saturday?
+  puts if date.saturday?
 end
 
-puts unless last_day.saturday?
+puts unless last_date.saturday?
 
 puts
