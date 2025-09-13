23b50dfac5384a420d2047f904541e784297c2fa
y-oji <yo-00331pc@outlook.com>
2025-09-13 19:29:31 +0900

    add ls

---
+++ 04.ls/ls.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+def acquire_files
+  Dir.glob('*').sort
+end
+
+def acquire_max_length(files)
+  max_length = 0
+  files.each { |file| max_length = file.length if file.length > max_length }
+  max_length
+end
+
+def create_array(files, columns)
+  count = files.size
+  rows = count / columns
+
+  rows += 1 if (count % columns).positive?
+
+  array = []
+  rows.times { array << [] }
+
+  files.each_with_index do |file, index|
+    row = index % rows
+    array[row] << file
+  end
+
+  array
+end
+
+def organize_file(array, column_length)
+  array.each do |row|
+    row.each do |file|
+      print file.ljust(column_length + 2)
+    end
+    puts
+  end
+end
+
+def display_files(files, columns)
+  max_length = acquire_max_length(files)
+  rows = create_array(files, columns)
+  organize_file(rows, max_length)
+end
+
+files = acquire_files
+display_files(files, 3)
