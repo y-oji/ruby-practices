43fa292c1e2791647f8715902d033614e258afc7
y-oji <yo-00331pc@outlook.com>
2025-12-14 13:41:13 +0900

    remove unnecessary *

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -13,7 +13,7 @@ OptionParser.new do |opts|
 end.parse!
 
 def acquire_files(options)
-  files = Dir.glob('*', *(options[:all] ? File::FNM_DOTMATCH : 0))
+  files = Dir.glob('*', options[:all] ? File::FNM_DOTMATCH : 0)
   files = files.reverse if options[:reverse]
   files
 end
