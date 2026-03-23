55a435d3a1ed2933adc9164c73507b2eaef7cac0
y-oji <yo-00331pc@outlook.com>
2026-03-24 03:47:59 +0900

    add -l option to display the detailed information

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -2,20 +2,75 @@
 # frozen_string_literal: true
 
 require 'optparse'
+require 'etc'
 
 COLUMNS = 3
 
-options = { all: false, reverse: false }
+options = { all: false, reverse: false, long: false }
 
 OptionParser.new do |opts|
   opts.on('-a') { options[:all] = true }
   opts.on('-r') { options[:reverse] = true }
+  opts.on('-l') { options[:long] = true }
 end.parse!
 
 def acquire_files(options)
   files = Dir.glob('*', options[:all] ? File::FNM_DOTMATCH : 0)
   files = files.reverse if options[:reverse]
   files
+end
+
+def file_type(file)
+  File.directory?(file) ? 'd' : '-'
+end
+
+PERMISSION_TABLE = {
+  '0' => '---',
+  '1' => '--x',
+  '2' => '-w-',
+  '3' => '-wx',
+  '4' => 'r--',
+  '5' => 'r-x',
+  '6' => 'rw-',
+  '7' => 'rwx'
+}.freeze
+
+def permission_string(stat)
+  mode = stat.mode.to_s(8)
+  permission_number = mode[-3..]
+  permission = ''
+  permission_number.each_char do |number|
+    permission += PERMISSION_TABLE[number]
+  end
+
+  permission
+end
+
+def put_long_format_in_one_line(file)
+  stat = File.lstat(file)
+  type = file_type(file)
+  permission = permission_string(stat)
+  nlink = stat.nlink.to_s.rjust(2)
+  owner = Etc.getpwuid(stat.uid).name
+  group = Etc.getgrgid(stat.gid).name
+  size = stat.size.to_s.rjust(5)
+  time = stat.mtime.strftime('%_m %2e %H:%M')
+
+  "#{type}#{permission} #{nlink} #{owner} #{group} #{size} #{time} #{file}"
+end
+
+def display_long_format(files)
+  total_blocks = 0
+
+  files.each do |file|
+    stat = File.lstat(file)
+    total_blocks += stat.blocks
+  end
+
+  puts "total #{total_blocks}"
+  files.each do |file|
+    puts put_long_format_in_one_line(file)
+  end
 end
 
 def acquire_max_length(files)
@@ -53,4 +108,9 @@ def display_files(files, columns)
 end
 
 files = acquire_files(options)
-display_files(files, COLUMNS)
+
+if options[:long]
+  display_long_format(files)
+else
+  display_files(files, COLUMNS)
+end
